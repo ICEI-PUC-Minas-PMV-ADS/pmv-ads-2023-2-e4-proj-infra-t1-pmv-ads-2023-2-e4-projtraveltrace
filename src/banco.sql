b5cc0b7f5ed36964d8812a15fd2488ace4683015
Laura Andrade <lauraandrad05@gmail.com>
2023-09-04 09:47:54 -0300

    banco de dados

--- src/banco.sql
+++ src/banco.sql
@@ -1,7 +1,7 @@
 -- Criação da tabela de cadastro
 CREATE TABLE cadastro (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome_completo VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL
+    nome_completo VARCHAR(200) NOT NULL,
+    email VARCHAR(200) UNIQUE NOT NULL,
+    senha VARCHAR(8) NOT NULL
 );
