92df77b6e9329142c0a53202d700f270b94c7a47
Gabriel Silveira Ildefonso <101571743+gabriel-ildefonso@users.noreply.github.com>
2023-09-28 12:32:16 -0300

    Update authenticate.js

--- src/api/src/middlewares/authenticate.js
+++ src/api/src/middlewares/authenticate.js
@@ -1,51 +1,56 @@
-const jwt = require('jsonwebtoken')
-const authConfig = require('../config/auth.json')
+// Importa a biblioteca jsonwebtoken (JWT) para autenticação
+const jwt = require('jsonwebtoken');
 
+// Importa a configuração de autenticação do arquivo auth.json
+const authConfig = require('../config/auth.json');
+
+// Exporta o middleware de autenticação para uso em rotas protegidas
 module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization
+  // Obtém o cabeçalho de autorização da solicitação
+  const authHeader = req.headers.authorization;
 
-  if(!authHeader){
+  // Verifica se o cabeçalho de autorização está ausente
+  if (!authHeader) {
     return res.status(401).json({
-      error: true, 
-      message: 'Token não provido'
-    })
+      error: true,
+      message: 'Token não provido',
+    });
   }
 
-  //conferir se o token segue o padrão JWT (bearer   part.part.part)
-  const parts = authHeader.split(' ')
-  if(parts.length !==2){
+  // Divide o cabeçalho de autorização em partes (Bearer e o token)
+  const parts = authHeader.split(' ');
+
+  // Verifica se o token está no formato adequado (Bearer <token>)
+  if (parts.length !== 2) {
     return res.status(401).json({
-      error: true, 
-      message: 'Tipo de token inválido'
-    })
+      error: true,
+      message: 'Tipo de token inválido',
+    });
   }
 
-  const [scheme, token] = parts
-  if(scheme.indexOf('Bearer') !== 0){
+  const [scheme, token] = parts;
+
+  // Verifica se o esquema do token é "Bearer"
+  if (scheme.indexOf('Bearer') !== 0) {
     return res.status(401).json({
-      error: true, 
-      message: 'Token mal formatado'
-    })
+      error: true,
+      message: 'Token mal formatado',
+    });
   }
 
-  return jwt.verify(token, authConfig.secret, (err, decoded) =>{
-
-    console.log(err)
-    console.log(decoded)
-
-    if(err){
+  // Verifica a validade e autentica o token usando o segredo do auth.json
+  return jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if (err) {
       return res.status(401).json({
-        error: true, 
-        message: 'Token inválido/expirado'
-      })
+        error: true,
+        message: 'Token inválido/expirado',
+      });
     }
 
-    req.userLogged = decoded
+    // Anexa os dados do usuário decodificado à solicitação para uso posterior
+    req.userLogged = decoded;
 
-    console.log(err)
-    console.log(decoded)
-
-    return next()
-  })
-  
-}
+    // Continua o fluxo de execução para a próxima função/middleware
+    return next();
+  });
+};
