36e0b53ef2e01dcfac6ee453140fbd6fefc4739c
Gabriel Silveira Ildefonso <101571743+gabriel-ildefonso@users.noreply.github.com>
2023-09-28 12:29:49 -0300

    Update user.js

--- src/api/src/models/user.js
+++ src/api/src/models/user.js
@@ -1,40 +1,45 @@
-const mongoose = require('../database/index')
-const bcryptjs = require('bcryptjs')
+// Importa as bibliotecas necessárias
+const mongoose = require('../database/index');
+const bcryptjs = require('bcryptjs');
 
+// Define o esquema do modelo de usuário
 const UserSchema = new mongoose.Schema({
-  name:{
+  name: {
     type: String,
     required: true,
   },
-  email:{
+  email: {
     type: String,
     required: true,
     unique: true,
-    lowercase: true
+    lowercase: true,
   },
-  password:{
+  password: {
     type: String,
     required: true,
-    select: false,
+    select: false, // Não retorna a senha ao consultar o usuário
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
+// Função de hash da senha antes de salvar no banco de dados
 UserSchema.pre("save", async function (next) {
   try {
     const hash = await bcryptjs.hash(this.password, 10); // Use o número de salt rounds adequado (exemplo: 10)
     console.log(this);
     console.log(hash);
-    this.password = hash;
+    this.password = hash; // Substitui a senha original pelo hash gerado
     next();
   } catch (error) {
     next(error);
   }
 });
 
-const User = mongoose.model('User', UserSchema)
+// Cria o modelo de usuário com base no esquema definido
+const User = mongoose.model('User', UserSchema);
 
-module.exports = User
+// Exporta o modelo para uso em outras partes do aplicativo
+module.exports = User;
