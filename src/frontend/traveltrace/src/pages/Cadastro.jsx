5cea69ab083d2889b10023e11fdc53e1e12da17b
fcostar <fisicocosta@gmail.com>
2023-10-17 22:10:42 -0300

    Implementação de navegação entre telas

--- src/frontend/traveltrace/src/pages/Cadastro.jsx
+++ src/frontend/traveltrace/src/pages/Cadastro.jsx
@@ -1,5 +1,16 @@
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Cadastro() {
-    return (<h1>Cadastro</h1>)
+    return (
+    
+        <>
+        <h1>Cadastro</h1>
+        <Link to="/login">Go Back</Link>
+      </>
+    
+    
+    
+    
+    )
 }
--- src/frontend/traveltrace/src/pages/Login.jsx
+++ src/frontend/traveltrace/src/pages/Login.jsx
@@ -1,4 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 export default function Cadastro() {
-    return (<h1>Login</h1>)
+    return (
+        <>
+        <h1>Login</h1>
+ 
+        <p><Link to="/cadastro">Ainda não tem conta? Cadastre-se!</Link></p>
+        
+        <p><Link to="/home">Voltar para Home!</Link></p>
+
+        
+      </>
+    
+    )
 }
