696d014e89e080c2c775712768fa6aee75db5b57
fcostar <fisicocosta@gmail.com>
2023-11-19 17:11:57 -0300

    Estilo do front mobile Login

--- src/frontendMobile/TravelTrace/src/theme/index.js
+++ src/frontendMobile/TravelTrace/src/theme/index.js
@@ -5,8 +5,12 @@ export const theme = {
   colors: {
     ...DefaultTheme.colors,
     text: '#1e212b',
-    primary: '#ffc800',
+    primary: '#25B8B5',
     secondary: '#1F2732',
     error: '#ED1C24',
   },
 }
+//   text: '#1e212b',
+//primary: '#ffc800', amarelo
+//secondary: '#1F2732',
+//error: '#ED1C24',
